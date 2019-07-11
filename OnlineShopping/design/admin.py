e009025bbbb2d5a12526f807e37bedbcc384e79d
papry <52271203+papry@users.noreply.github.com>
2019-07-11 23:12:37 +0600

    line added

--- OnlineShopping/design/admin.py
+++ OnlineShopping/design/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from.models import category
+from.models import Admin
+from.models import Product
+from.models import Supplier
+from.models import Payment
+from.models import Customer
 
-# Register your models here.
+admin.site.register(category)
+admin.site.register(Supplier)
+admin.site.register(Product)
+admin.site.register(Admin)
+admin.site.register(Customer)
+admin.site.register(Payment)
